a1b35bcc9d3ca67e546298b6c4f0e31c231522a7
developersrinu <orsusrinu606@gmail.com>
2023-08-12 05:54:51 +0530

    'fff'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  // num1
+  const [number1, setNumber1] = useState("");
+  // num 2
+  const [number2, setNumber2] = useState("");
+  // error msg
+  const [errorMessage, setErrorMessage] = useState("");
+  // result 
+  const [result, setResult] = useState("");
+  // succces
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const add = () => {
+    if (validateInputs()) {
+      const sum = parseFloat(number1) + parseFloat(number2);
+      setResult(sum);
+      setSuccessMessage("Calculation successful");
+      setErrorMessage("");
+    }
+  };
+
+  const subtract = () => {
+    if (validateInputs()) {
+      const difference = parseFloat(number1) - parseFloat(number2);
+      setResult(difference);
+      setSuccessMessage("Calculation successful");
+      setErrorMessage("");
+    }
+  };
+
+  const multiply = () => {
+    if (validateInputs()) {
+      const product = parseFloat(number1) * parseFloat(number2);
+      setResult(product);
+      setSuccessMessage("Calculation successful");
+      setErrorMessage("");
+    }
+  };
+
+  const divide = () => {
+    if (validateInputs() && parseFloat(number2) !== 0) {
+      const quotient = parseFloat(number1) / parseFloat(number2);
+      setResult(quotient);
+      setSuccessMessage("Calculation successful");
+      setErrorMessage("");
+    } else {
+      setErrorMessage("Cannot divide by zero");
+      setSuccessMessage("");
+    }
+  };
+
+  // checking for valid input
+  const validateInputs = () => {
+    if (number1.trim() === "" || number2.trim() === "") {
+      setErrorMessage("Please enter both numbers");
+      setSuccessMessage("");
+      return false;
+    }
+    if (!/^-?\d*\.?\d+$/.test(number1) || !/^-?\d*\.?\d+$/.test(number2)) {
+      setErrorMessage("Invalid number format");
+      setSuccessMessage("");
+      return false;
+    }
+    setErrorMessage("");
+    return true;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <input
+        type="text"
+        placeholder="Enter number 1"
+        value={number1}
+        onChange={(e) => setNumber1(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Enter number 2"
+        value={number2}
+        onChange={(e) => setNumber2(e.target.value)}
+      />
+
+      <button onClick={add}>+</button>
+      <button onClick={subtract}>-</button>
+      <button onClick={multiply}>*</button>
+      <button onClick={divide}>/</button>
+
+      {errorMessage && <div className="error">{errorMessage}</div>}
+      {successMessage && <div className="success">{successMessage}</div>}
+      {result && <div className="result">Result: {result}</div>}
     </div>
   );
 }
